21aa6a04f31cf90e850a7853a31eef42859e04df
Roman Marinich <romarinich@gmail.com>
2022-11-16 15:24:01 +0200

    event system

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
+
 namespace Hazel {
     Application::Application()
     {
@@ -10,9 +13,11 @@ namespace Hazel {
     }
     void Application::Run()
     {
+        WindowResizeEvent e(1280,720);
+
+        HZ_TRACE(e);
+
         while (true);
-        {
-            
-        }
+        
     }
 }

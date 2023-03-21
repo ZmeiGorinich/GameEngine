7a25e3b6471ba2f5f8b7002a95557e1b5c48e8e6
Roman Marinich <romarinich@gmail.com>
2023-03-21 10:52:30 +0200

    key and mouse codes.

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -4,6 +4,9 @@
 #include "Hazel/Application.h"
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
+#include "Hazel/Input.h"
+#include "Hazel/KeyCodes.h"
+#include "Hazel/MouseButtonCodes.h"
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 

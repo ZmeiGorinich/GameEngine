0b62bbb8cebfc41c42a8f9fdc7621ffe8e3710fe
Roman Marinich <romarinich@gmail.com>
2023-04-02 13:21:02 +0300

    Added orthographic camera.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -13,7 +13,7 @@ namespace Hazel {
 
     Application* Application::s_Instance = nullptr;
 
-    Application::Application()
+    Application::Application() : m_Camera(-1.6f, 1.6f, -0.9f, 0.9f)
     {
         HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
         s_Instance = this;
@@ -73,6 +73,8 @@ namespace Hazel {
 			layout(location = 0) in vec3 a_Position;
 			layout(location = 1) in vec4 a_Color;
 
+            uniform mat4 u_ViewProjection;
+
 			out vec3 v_Position;
 			out vec4 v_Color;
 
@@ -80,7 +82,7 @@ namespace Hazel {
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -167,14 +169,15 @@ namespace Hazel {
             RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
             RenderCommand::Clear();
 
+            m_Camera.SetPosition({ 0.5f, 0.5f, 0.0f });
+            m_Camera.SetRotation(45.0f);
+            Renderer::BeginScene(m_Camera);
 
-            Renderer::BeginScene();
+            //m_BlueShader->Bind();
+            Renderer::Submit(m_BlueShader, m_SquareVA);
 
-            m_BlueShader->Bind();
-            Renderer::Submit(m_SquareVA);
-
-            m_Shader->Bind();
-            Renderer::Submit(m_VertexArray);
+            //m_Shader->Bind();
+            Renderer::Submit(m_Shader, m_VertexArray);
            
             Renderer::EndScene();
             for (Layer* layer : m_LayerStack)

b2f4268bb6de75acaad98a7ba7c6e84c0176b4d1
Roman Marinich <romarinich@gmail.com>
2023-04-03 13:34:36 +0300

    add color

--- Hazel/src/Hazel/Renderer/Shader.h
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -11,9 +11,10 @@ namespace Hazel {
         Shader(const std::string& vertexSrc, const std::string& fragmentSrc);
         ~Shader();
 
-        void Bind() const;
-        void Unbind() const;
+        virtual void Bind() const;
+        virtual void Unbind() const;
 
+        void UploadUniformFloat4(const std::string& name, const glm::vec4& values);
         void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
     private:
         uint32_t m_RendererID;
